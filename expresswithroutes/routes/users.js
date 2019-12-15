6d9c66e40787f07e638ccc86439ba3aae036766f
serkan <serkans@gmail.com>
2019-12-15 12:48:43 +0300

    templating engine

--- expresswithroutes/routes/users.js
+++ expresswithroutes/routes/users.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const router = require('express').Router();
 
+
+const users = {
+	'user1', 'user2', 'user3'
+}
+
 router.get('/users', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
+    //res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
+    res.render('users', {users});
 })
 
 module.exports = router
